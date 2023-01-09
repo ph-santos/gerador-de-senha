ae8e575d14453a9c3f4017ab47a41f5f5695a20e
Pedro Henrique <phenrique.camara@hotmail.com>
2023-01-08 23:15:28 -0300

    Correções no JavaScript

--- assets/index.js
+++ assets/index.js
@@ -1,32 +1,31 @@
-const sliderElement = document.querySelector('#slider');
-const buttonElement = document.querySelector('#button');
-const sizePasswrd = document.querySelector('#valor');
+const spanValue = document.querySelector('#valor');
+const sliderValue = document.querySelector('#slider');
 const password = document.querySelector('#password');
-const contatinerPassword = document.querySelector('#container-password');
+const containerPassword = document.querySelector('#container-password');
+const button = document.querySelector('#button');
 
-let charset = 'abcdefghijklmnopqrstuvwxzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%&*';
-let novaSenha = '';
+const characters = 'abcdefghijklmnopqrstuvwxzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%&*'; 
+let newPassword = '';
 
-sizePasswrd.innerHTML = sliderElement.value;
+spanValue.innerHTML = sliderValue.value;
 
-sliderElement.oninput = function () {
-    sizePasswrd.innerHTML = this.value;
+sliderValue.oninput = function () {
+    spanValue.innerHTML = this.value;
 }
 
-buttonElement.addEventListener('click', function generetePassword() {
+button.addEventListener('click', function () {
     let pass = '';
-    
-    for (let i = 0, n = charset.length; i < sliderElement.value; i++) {
-        pass += charset.charAt(Math.floor(Math.random() * n));
+
+    for (let i = 0, n = characters.length; i < sliderValue.value; i++) {
+        pass += characters.charAt(Math.floor(Math.random() * n));
     }
 
-    contatinerPassword.classList.remove('hide');
-    password.innerText = pass;
-
-    novaSenha += pass;
+    containerPassword.classList.remove('hide');
+    password.innerHTML = pass;
+    newPassword = pass;
 })
 
 password.addEventListener('click', function () {
+    navigator.clipboard.writeText(newPassword);
     alert('Senha copiada!');
-    navigator.clipboard.writeText(novaSenha);
 })
